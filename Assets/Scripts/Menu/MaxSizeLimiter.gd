cc8361ddd42c3552486605a108cc7374f9bc5245
Eluem <eulmmm@hotmail.com>
2024-12-17 09:30:11 -0500

    Updated main menu centering method
    - I didn't realize the VBoxContainer had an alignment property that could be set to "Center". Used that instead of my janky "CenteringBuffer" which was 2/3rds the size of the difference between the virtual logo and the true logo to fill the space.. I still wish there was a max_size property

--- Assets/Scripts/Menu/MaxSizeLimiter.gd
+++ Assets/Scripts/Menu/MaxSizeLimiter.gd
@@ -2,7 +2,6 @@ extends Control
 class_name MaxSizeLimiter
 
 @onready var virtualLogo:TextureRect = $"../../Virtual/Logo"
-@onready var centeringBuffer:Control = $"../CenteringBuffer"
 
 
 @export var maxSize:Vector2
@@ -20,7 +19,6 @@ func _process(_delta: float) -> void:
 	pass
 
 func limit_size() -> void:
-	centeringBuffer.custom_minimum_size.y = (virtualLogo.size.y-size.y) * (2.0/3.0)
 	if(!oversized && ((virtualLogo.size.x > maxSize.x && maxSize.x > -1) || (virtualLogo.size.y > maxSize.y && maxSize.y > -1))):
 		custom_minimum_size = maxSize
 		size_flags_vertical -= SIZE_EXPAND
