97e138dc348ecc95a77c9cedae36a320bce2a4ff
Eluem <eulmmm@hotmail.com>
2025-04-05 10:23:59 -0400

    Fixed boss health bar missing on mobile
    - Changed a bit of special case mobile code from shifting the position of a panel down to adding a margin to the top. This moves the player health bar down into the "display safe area" without pushing the health bar down off the screen

--- Assets/Scripts/UI/MobileScreenSafeHUD.gd
+++ Assets/Scripts/UI/MobileScreenSafeHUD.gd
@@ -1,6 +1,6 @@
 #Simple bit of code to bump the HUD down for mobile platforms so the HUD doesn't overlap the camera
 extends Control
-class_name MobileScreenSafeHUD
+class_name MobileScreenSafeHUDMargin
 
 var displaySafeArea:Rect2i
 
@@ -8,4 +8,6 @@ func _ready() -> void:
 	if(OS.has_feature("android")):
 		displaySafeArea = DisplayServer.get_display_safe_area()
 		#size = displaySafeArea.size
-		position = displaySafeArea.position
+		#position = displaySafeArea.position
+		add_theme_constant_override("margin_top", 10 + displaySafeArea.position.y)
+		add_theme_constant_override("margin_left", displaySafeArea.position.x)
